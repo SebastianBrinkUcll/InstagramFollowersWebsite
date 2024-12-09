8611d2884dc7c27f72fe912c28530218b23f0b70
Sebastian Brink <sebastian.brink@student.ucll.be>
2024-12-09 14:53:04 +0100

    update

--- main_insta_project/instaApp.py
+++ main_insta_project/instaApp.py
@@ -84,4 +84,4 @@ def upload_files():
     return render_template('unfollowers.html', unfollowers=sorted(unfollowers))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
